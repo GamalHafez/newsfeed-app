f9ef0f33e4b4d46d8f43c2251a291c376816342a
GamalHafez <gamal.k.hafez@gmail.com>
2025-11-15 10:28:22 +0200

    feat: Replace sample data with News API call

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,30 @@
 import { Header, Layout, NewsFeed } from "@/components";
+import { useEffect, useState } from "react";
 
-const sampleArticles = [
-  {
-    title: "Test News 1",
-    description: "This is a test news description 1.",
-    image: "https://placehold.co/150",
-    author: "John Doe",
-    publishedAt: "2023-03-01T12:00:00Z",
-  },
-  {
-    title: "Test News 2",
-    description: "This is a test news description 2.",
-    image: "https://placehold.co/150",
-    author: "Jane Smith",
-    publishedAt: "2023-04-01T12:00:00Z",
-  },
-  {
-    title: "Test News 3",
-    description: "This is a test news description 3.",
-    image: "https://placehold.co/150",
-    author: "John Doe",
-    publishedAt: "2023-05-01T12:00:00Z",
-  },
-  {
-    title: "Test News 4",
-    description: "This is a test news description 4.",
-    image: "https://placehold.co/150",
-    author: "Jane Smith",
-    publishedAt: "2023-06-01T12:00:00Z",
-  },
-];
+const apiUrl = `https://newsapi.org/v2/everything?q=مصر&language=ar&apiKey=${
+  import.meta.env.VITE_NEWS_API_KEY
+}`;
+
+async function loadData() {
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  return data.articles.map((article) => {
+    const { title, description, author, publishedAt } = article;
+    return { title, description, author, publishedAt };
+  });
+}
 
 export default function App() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    loadData().then(setArticles);
+  }, []);
+
   return (
     <Layout>
       <Header />
-      <NewsFeed articles={sampleArticles} />
+      <NewsFeed articles={articles} />
     </Layout>
   );
 }
